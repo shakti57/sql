5d7b8de866bea2f0508cdcba49984a6e60cde26e
Shakti Ranjan Mohanta <153294330+shakti57@users.noreply.github.com>
2024-02-14 11:39:50 +0530

    Add files via upload

---
+++ git1.sql
@@ -0,0 +1,16 @@
+show databases;
+create database shakti;
+use shakti
+;
+create table bipul(
+name2 varchar(20),
+age varchar(20),
+add2 varchar(20));
+select*from bipul;
+
+drop database shakti;
+
+
+insert into bipul
+values('ram','27','db');
+
